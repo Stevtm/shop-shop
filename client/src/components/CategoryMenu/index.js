31ff9c0146b31832537c1a65b96759e81c6a6f5f
Stevtm <steventmaddison@outlook.com>
2021-07-12 23:28:45 -0400

    update global category state

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_CATEGORIES } from '../../utils/queries';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import {
+	UPDATE_CATEGORIES,
+	UPDATE_CURRENT_CATEGORY,
+} from "../../utils/actions";
+import { useStoreContext } from "../../utils/GlobalState";
 
-function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+function CategoryMenu() {
+	const [state, dispatch] = useStoreContext();
 
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            setCategory(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+	const { categories } = state;
+
+	const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+	useEffect(() => {
+		// if categoryData exists or has been changed from the response of useQuery, then run dispatch()
+		if (categoryData) {
+			// execute dispatch function with action object indicating the type of action and the data to set the state for categories to
+			dispatch({
+				type: UPDATE_CATEGORIES,
+				categories: categoryData.categories,
+			});
+		}
+	}, [categoryData, dispatch]);
+
+	const handleClick = (id) => {
+		dispatch({
+			type: UPDATE_CURRENT_CATEGORY,
+			currentCategory: id,
+		});
+	};
+
+	return (
+		<div>
+			<h2>Choose a Category:</h2>
+			{categories.map((item) => (
+				<button
+					key={item._id}
+					onClick={() => {
+						handleClick(item._id);
+					}}
+				>
+					{item.name}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryMenu;
